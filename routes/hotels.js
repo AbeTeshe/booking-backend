5ae9aa04c2db05d4c9da08bdc655dc8291397a36
abebe teshome <abebe.teshome@CNETERP.COM>
2022-09-26 13:45:16 +0300

    Reserve a room(api)

--- routes/hotels.js
+++ routes/hotels.js
@@ -20,7 +20,7 @@ router.get("/find/:id", getHotel)
 router.get("/", getHotels)
 router.get("/countByCity", countByCity)
 router.get("/countByType", countByType)
-router.get("/room?:id", getHotelRooms);
+router.get("/room/:id", getHotelRooms);
 
 
 
